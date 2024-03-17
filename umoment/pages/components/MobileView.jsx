041268ef42b31538198d82ee4a243c9e004f12c9
Pranav Ramesh <laphatize@protonmail.com>
2024-03-17 13:42:39 -0400

    fun

--- umoment/pages/components/MobileView.jsx
+++ umoment/pages/components/MobileView.jsx
@@ -354,7 +354,7 @@ function fetchCoordinates(address, apiKey) {
 
 
             <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-neutral-900 opacity-50">
-                <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+                <div className="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
                     <button type="button" className="hidden hover:opacity-100 inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group-hover:text-blue-600 group">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" className='text-white' />
@@ -373,14 +373,14 @@ function fetchCoordinates(address, apiKey) {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" className='text-white' />
                         </svg>
 
-                        <span className="mt-1 text-sm text-white  group-hover:text-blue-600 ">Add</span>
+                        <span className="mt-1 text-sm text-white  group-hover:text-blue-600 ">Post Event</span>
                     </button>
                     <button type="button" className="hover:opacity-100 inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group" onClick={() => handleGroupPopupOpen()}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                         </svg>
 
-                        <span className="mt-1 text-sm text-white group-hover:text-blue-600 ">Groups</span>
+                        <span className="mt-1 text-sm text-white group-hover:text-blue-600 ">Create Group</span>
                     </button>
                 </div>
             </div>
