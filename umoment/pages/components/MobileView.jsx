69ab5c5062167ac0b80fac49da3caa72ae0afd01
Pranav Ramesh <laphatize@protonmail.com>
2024-03-17 13:29:15 -0400

    test

--- umoment/pages/components/MobileView.jsx
+++ umoment/pages/components/MobileView.jsx
@@ -355,14 +355,14 @@ function fetchCoordinates(address, apiKey) {
 
             <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-neutral-900 opacity-50">
                 <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-                    <button type="button" className="hover:opacity-100 inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group-hover:text-blue-600 group">
+                    <button type="button" className="hidden hover:opacity-100 inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group-hover:text-blue-600 group">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" className='text-white' />
                         </svg>
 
                         <span className="mt-1 text-sm text-white  group-hover:text-blue-600">Today</span>
                     </button>
-                    <button type="button" className="hover:opacity-100 inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group">
+                    <button type="button" className="hover:opacity-100 hidden inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-800  group">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-white">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" className='text-white w-6 h-6' />
                         </svg>
